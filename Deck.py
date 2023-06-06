e745f04b3060307f259277efbe51df04564a4dd6
MLambi-Dover <101574810+MLambi-Dover@users.noreply.github.com>
2023-06-06 08:59:52 -0400

    added blackjack dict

--- Deck.py
+++ Deck.py
@@ -9,6 +9,9 @@ class Deck():
     STANDARD_DICT = {'Ace':1, '2':2, '3':3, '4':4, '5':5, 
                      '6':6, '7':7, '8':8, '9':9, '10':10, 
                      'Jack':11, 'Queen':12, 'King':13}
+    BLACKJACK_DICT = {'Ace':1, '2':2, '3':3, '4':4, '5':5, 
+                     '6':6, '7':7, '8':8, '9':9, '10':10, 
+                     'Jack':10, 'Queen':10, 'King':10}
     
     def __init__(self, window, rankValueDict=STANDARD_DICT):
         # other decks need to be made
